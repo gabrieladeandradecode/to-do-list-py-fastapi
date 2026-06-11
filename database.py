139de7a090cc3970327a51fd344b4d10248c8b68
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-06-11 18:26:40 -0300

    chore: .env

--- database.py
+++ database.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 print(DATABASE_URL)
-# DATABASE_URL = "postgresql://postgres:home@localhost/tasks"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
